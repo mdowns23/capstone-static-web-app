99b21e6176b5dd2bb7a34756a295361b5f521e99
mdowns23 <mdowns23@hotmail.com>
2023-04-27 17:33:49 -0700

    gas station displaying
    gas station info with backend

--- api/data_function/__init__.py
+++ api/data_function/__init__.py
@@ -1,16 +1,79 @@
 import logging
 import json
+import requests
 import azure.functions as func
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    googleKey = 'notkey'
-    if req.method == "GET":
-        googleKey = os.environ.get('REACT_APP_GOOGLE_MAPS_API_KEY')
-        return func.HttpResponse(json.dumps({"key": googleKey}))
+    text = "not changed"
+    if req.method == "POST":
+        body = str(req.get_json())
+        body1 = req.get_json()
+        lat = float(body1.get('lat'))
+        lat-=0.0005
+        #lat-=0.0347097444
+        long = float(body1.get('lng'))
+        long-=0.0005
+        #long-=0.0702095032
+        lat1 = float(lat)
+        lat1+=0.0005
+        #lat1+=0.0347097444
+        long1 = float(long)
+        long1+=0.0005
+        #long1+=0.0702095032
+        #textJ = json.loads(text)
+        #print("TEXT: " + body)
+        gType = str(body1.get('type'))
+        #print("TYPE: " + gType)
+        if (gType[0] == 'R'):
+            gasT = 1
+        elif (gType[0] == 'M'):
+            gasT = 2
+        elif (gType[0] == 'P'):
+            gasT = 3
+        else:
+            gasT = 4
+        
+        
+        url = 'https://www.gasbuddy.com/gaspricemap/map'
+        #url = 'https://www.gasbuddy.com/GBPriceWebService/GBPriceService.asmx/GetPrices'
+        #print("URL: " + url)
+        headers = {
+            'Content-Type': 'application/json'
+        }
+        data1 = {
+            'fuelTypeId': str(gasT),
+            'height': 600,
+            'maxLat': lat1,
+            'maxLng': long1,
+            'minLat': lat,
+            'minLng': long,
+            'width': 818
+        }
+        #response = requests.post(url, data=data1)
+        response = requests.post(url, data=data1)
+        res = json.loads(response.content)
+        stations = res.get("primaryStations")
+        id = stations[0].get("id")
+        price = stations[0].get("price")
+        lat3 = float(body1.get('lat'))
+        long3 = float(body1.get('lng'))
+        url2 = 'https://www.gasbuddy.com/gaspricemap/county?lat={lati1}&lng={longi1}&usa=true'.format(lati1=lat3, longi1=long3)
+        data = {
+            "fuelTypeId": "1",
+            "id": id
+        }
+        response2 = requests.post(url2)
+        res2 = json.loads(response2.content)
+        print(response2.content)
+        if(price == '--'):
+            price = str(res2[0].get("Price"))
+            price+= " (AVG in County)"
+        #print("RES2: " + response2.text)
+        return func.HttpResponse(json.dumps(price))
+    else:
+        return func.HttpResponse(json.dumps(text))
 
     
--- road-trip-planner/src/App.js
+++ road-trip-planner/src/App.js
@@ -35,9 +35,26 @@ function App() {
   //variable: destation
   const [destination, setDestination] = useState()
 
+  const [gasType, setGasType] = useState()
+
+  const [Mpg, setMpg] = useState()
+
+  const [TankSize, setTankSize] = useState()
+
+  const [CurrentSize, setCurrentSize] = useState()
+
+
   const [isShown, setIsShown] = useState(false);
   //const [Distance, setDistance] = useState()
   //const [Duration, setDuration] = useState()
+
+  
+
+  const [ValueArray, setValueArray] = useState(new Set())
+
+  
+
+  const [gasInformaiton, setGasInformation] = useState([])
 
 //setting the starting location to update
   const handleSatringLocationChange = (event) => {
@@ -48,6 +65,22 @@ function App() {
     //console.log("User location" + e.target.value);
   };
 
+  const handleCurrentSize= (event) => {
+    setCurrentSize(event.target.value);
+  }
+
+  const handleMPG= (event) => {
+    setMpg(event.target.value);
+  }
+
+
+  const handleTanksize= (event) => {
+    setTankSize(event.target.value);
+  }
+
+  const handleSelect= (event) => {
+    setGasType(event.target.value);
+  }
   //setting the diestentaion to update
   const handleDestinationChange = (e) => {
     setDestination(e.target.value);
@@ -58,6 +91,10 @@ function App() {
     e.preventDefault();
     setStartingLocation('')
     setDestination('')
+    setCurrentSize('')
+    setGasType('')
+    setMpg('')
+    setTankSize('')
     
     setIsShown(true);
     // console.log('Map Starting Location: ', startingLocation);
@@ -87,7 +124,7 @@ function App() {
   /** @type React.MutableRefObject<HTMLInputElement> */
   const destinationRef = useRef()
 
-  
+  let GasInformation = []
 
   if(!isLoaded){
     return(<></>)
@@ -122,25 +159,29 @@ function App() {
     setDuration(results.routes[0].legs[0].duration.text)
     var polyline = require('google-polyline')
     waypoints = polyline.decode(results.routes[0].overview_polyline)
-    console.log(waypoints)
+    //console.log(waypoints[waypoints.length-1])
+    //console.log(await distanceCalc(48,122,47,122.5))
+    //console.log("mpg: " + Mpg + " ts: " + TankSize + " gt: " + gasType + " cs: " + CurrentSize )
     const polygonCoords = polygonPoints()
     const polygonBound = new google.maps.Polygon({
       paths: polygonCoords,
       strokeColor: '#FF0000',
-      strokeOpacity: 0.8,
+      strokeOpacity: 0,
       strokeWeight: 2, 
       fillColor: '#FF0000',
-      fillOpacity: 0.35
+      fillOpacity: 0
     });
     setGasPolygon(polygonBound)
     polygonBound.setMap(map)
     getStations(polygonBound)
+    
   }
 
   function clearRoute() {
     setDirectionResponse(null)
     setDistance('')
     setDuration('')
+    distCoords.length = 0
     originRef.current.value = ''
     destinationRef.current.value = ''
     if(gasPolygon != null){
@@ -155,6 +196,10 @@ function App() {
       gasMarkerArray[i].setMap(null)
     }
     setGasMarkerArray([])
+    setGas = new Set()
+    setValueArray(new Set())
+    GasInformation.length = 0
+
   }
 
   function polygonArray(latitude){
@@ -189,9 +234,27 @@ function App() {
     return FullPoly
   }
 
+  var distCoords = []
+  let setGas = new Set()
+  let GasSet = []
+  
   function getStations(pBounds){
     let gasMarkers = []
+    var currTankSize = 0
+    if (CurrentSize === "TFive"){
+      currTankSize = TankSize*.25
+    } 
+    else if(CurrentSize === "Fifty"){
+      currTankSize = TankSize *.5
+    }
+    else if(CurrentSize === "SFive"){
+      currTankSize = TankSize*.75
+    }
+    else{ currTankSize = TankSize}
+    distCoords.push([waypoints[0][0], waypoints[0][1]])
+    var distTrack = 0
     const service = new google.maps.places.PlacesService(map)
+    const searchPromises = [];
     for (let i = 0; i < waypoints.length; i+=40){
       service.nearbySearch({
         location:{ lat: waypoints[i][0], lng:waypoints[i][1]},
@@ -199,20 +262,126 @@ function App() {
         type: ['gas_station']
       }, callback);
 
-      function callback(results, status){
+
+      async function callback(results, status){
         if(status === google.maps.places.PlacesServiceStatus.OK){
           for (var j = 0; j < results.length; j++){
             if(google.maps.geometry.poly.containsLocation(results[j].geometry.location,pBounds) === true){
-              gasMarkers.push(new google.maps.Marker({
-                position: results[j].geometry.location, map, 
-                title: 'Hello World!'
-              }))
+              //console.log("GEO: " + results[j].geometry.location.lat())
+              currTankSize = currTankSize - ((await distanceCalc(distCoords[distTrack][0], distCoords[distTrack][1], results[j].geometry.location.lat(), results[j].geometry.location.lng() )/Mpg))
+              var gasLvl = currTankSize/TankSize
+              
+              //console.log("LEVEL: " + gasLvl)
+              gasLvl = Math.round(gasLvl *10) / 10
+              //console.log(results[j]['vicinity'])
+              //console.log(results[j]['plus_code']['compound_code'])
+              if (gasLvl <= .5){
+                distCoords.push([results[j].geometry.location.lat(), results[j].geometry.location.lng()])
+                distTrack+=1
+                currTankSize = TankSize
+                gasMarkers.push(new google.maps.Marker({
+                  position: results[j].geometry.location, map, 
+                  title: results[j]['name']
+                }))
+
+                if(!setGas.has(results[j]['vicinity'])){
+                  var price = getGasStationInfo(results[j].geometry.location.lat(), results[j].geometry.location.lng())
+                }
+
+                var info = {
+                  name: results[j]['name'],
+                  vicinity: results[j]['vicinity'],
+                  price: await price
+                }
+                if(!setGas.has(results[j]['vicinity'])){
+                  GasInformation.push(info);
+                  setGas.add(results[j]['vicinity'])
+                  console.log("INLOOP:" + GasInformation);
+                  GasSet = Array.from(new Set(GasInformation))
+                  setValueArray(GasSet)
+                }
+                
+                
+                
+              }
             }
           }
         }
       }
     }
     setGasMarkerArray(gasMarkers)
+    //getGasStationInfo()
+    //console.log("ARRAY: " + GasInformation )
+    
+    console.log("SET" + ValueArray);
+    //console.log("Value in the array: "+ValueArray.length);
+  }
+  //console.log("Variable data "+GasInformation);
+  //console.log("UseState" + ValueArray);
+
+   //    GasInformation.push({name : results[j]['name'], vicinity :results[j]['vicinity']})
+              //     console.log("Zie of the value" +GasInformation)
+              // //  // setValueArray([GasInformation])
+              //     console.log("ARRAY: " + GasInformation[j]['name'] +" " + GasInformation[j]['vicinity'])
+              // //    //console.log(ValueArray)
+
+  async function getGasStationInfo(lat, long) {
+    //for (var i = 1; i < distCoords.length ; i++){
+      //console.log("COORDS: " + distCoords)
+      const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ 
+          lat: lat,
+          lng: long,
+          type: gasType })};
+
+      const response = await fetch('api/data_function', requestOptions);
+      const data = await response.json();
+      console.log("DATA:" + data)
+      return data
+    //}
+  }
+
+  async function distanceCalc(lat1, long1, lat2, long2){
+    const pt1 = {lat: lat1, lng: long1}
+    const pt2 = {lat: lat2, lng: long2}
+
+    let distanceService = new google.maps.DirectionsService()
+    const dist = await distanceService.route({
+      origin: pt1,
+      destination: pt2, 
+      travelMode: google.maps.TravelMode.DRIVING})
+    
+    var miles = dist.routes[0].legs[0].distance.value
+    //console.log(dist.routes[0].legs[0].distance.text)
+    miles = miles/1609.34
+    return miles
+
+
+  }
+
+
+  //console.log("The vlaue of the ValueArray: " + ValueArray)
+  // console.log("Beginning")
+
+  //   console.log("End")
+
+  // console.log("Gas Size" + GasInformation.length)
+  // const gasListItems = GasInformation.map((gas, index) => (
+  //   <li key={index}>
+  //     <div>Name: {gas.name}</div>
+  //     <div>Address: {gas.vicinity}</div>
+  //   </li>
+  // ));
+
+  // console.log(gasListItems)
+
+  //console.log("Value array:" + ValueArray);
+    // console.log(renderList);
+  //console.log(distCoords[0][1])
+  if(directionResponse){
+    //console.log(distCoords);
   }
 
    return (   
@@ -240,6 +409,42 @@ function App() {
               </tr>
             </tbody>
           </table>
+
+          <div class ='gasInputs'>
+          <div class = 'gasSide'>
+
+            <select value={gasType} onChange={handleSelect}>
+              <option value="">GasType</option>
+              <option value="Regular">Regular</option>
+              <option value="Midgrade">MidGrade</option>
+              <option value="Premium">Premium</option>
+              <option value="Diesel">Diesel</option>
+            </select>
+
+          </div>
+          
+
+          <div class = 'mpgside'>
+            <input type="text" value={Mpg} placeholder='Mpg' onChange={handleMPG} />
+          </div>
+
+          <div class = 'Tankside'>
+            <input type="text" value={TankSize} placeholder='Tank Size' onChange={handleTanksize} />
+          </div>
+          
+          <div class = 'CurrentGas'>
+
+            <select value={CurrentSize} onChange={handleCurrentSize}>
+                <option value="">Current Fuel</option>
+                <option value="TFive">25%</option>
+                <option value="Fifty">50%</option>
+                <option value="SFive">75%</option>
+                <option value="Full">100%</option>
+            </select>
+          </div>
+        </div>
+
+        
           <div className='button'>
             <button type="submit" onClick={calculateRoute}>calculate</button>
             <button type="submit" onClick={clearRoute}>Clear</button>
@@ -251,9 +456,10 @@ function App() {
                          onLoad={map => setMap(map)}>
                 {/*Display markers*/}
               </GoogleMap>
-              {directionResponse && (
-                 <div style={{ textAlign: "center" }}>
-                 <table class = 'distance'>
+              {directionResponse && gasMarkerArray && (
+                //  <div style={{ textAlign: "center" }}>
+                <div>
+                 {/* <table class = 'distance'>
                    <thead>
                      <tr class = 'inner'>
                        <th>Distance: {distance}</th>
@@ -262,12 +468,49 @@ function App() {
                    </thead>
                    <tbody>
                    </tbody>
-                 </table>
+                 </table> */}
+                 <div>
+              <h2>Gas Stations</h2>
+              <ul>{Array.from(ValueArray).map((dist, index) => {
+                  //console.log("Length value array" +  ValueArray.length)
+                  //console.log("Name: "+ dist.name +" Vicinity " + dist.vicinity)
+                  return(<li key={index}>{dist.name} - {dist.vicinity} - {dist.price}</li>)
+                })}</ul>
+            </div>
+
+                {/* <div>
+                  <h2>Gas Stations</h2>
+                  <table>
+                    <thead>
+                      <tr>
+                        <th>Name</th>
+                        <th>Vicinity</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      { ValueArray.length > 0 && ValueArray.map((dist, index) => {
+                        console.log("Name: "+ dist.name +" Vicinity " + dist.vicinity)
+                        return(
+                          <tr key={index}>
+                            <td>{dist.name}</td>
+                            <td>{dist.vicinity}</td>
+                          </tr>
+                        )
+                      })}
+                    </tbody>
+                  </table>
+                </div> */}
+                
                </div>
                 )}
             </Box>
+          
+           
           </div>
         </form>
+
+
+
       </main>
   //   return null
 
